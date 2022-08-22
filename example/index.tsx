47f79859dfdde644033ae9697fe18ec75d522892
Manaz PR <manas0096@gmail.com>
2022-08-22 22:15:24 +0530

    minor fix

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Button } from 'finity-ui';
 const App = () => {
-  return (
-    <div>
-      <Button variant="secondary">Connect Wallet</Button>
-    </div>
-  );
+  return <div></div>;
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
