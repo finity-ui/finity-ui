6a8159f91f1b23270a5450e646c8ae6d3d46d990
Manaz PR <manas0096@gmail.com>
2022-08-23 14:57:42 +0530

    Update v0.0.2

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,8 @@
-import React, { FC, HTMLAttributes, ReactChild } from 'react';
-
-export interface Props extends HTMLAttributes<HTMLDivElement> {
-  /** custom content, defaults to 'the snozzberries taste like snozzberries' */
-  children?: ReactChild;
-}
-
-// Please do not use types off of a default export module or else Storybook Docs will suffer.
-// see: https://github.com/storybookjs/storybook/issues/9556
-/**
- * A custom Thing component. Neat!
- */
-export const Thing: FC<Props> = ({ children }) => {
-  return <div>{children || `the snozzberries taste like snozzberries`}</div>;
-};
+export { ConnectWalletButton } from './ConnectWalletButton';
+export { ConnectionLoadingModal } from './ConnectionLoadingModal';
+export { ConnectionModal } from './ConnectionModal';
+export { LoadingSection } from './LoadingSection';
+export { Notifications } from './Notifications';
+export { Spinner } from './Spinner';
+export { VerificationLoadingModal } from './VerificationLoadingModal';
+export { WaitingNetworkModal } from './WaitingNetworkModal';
