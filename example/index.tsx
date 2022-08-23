e31fe550b19a7b47adadc5d97900877d64fc2220
Manaz PR <manas0096@gmail.com>
2022-08-23 14:57:28 +0530

    Update v0.0.2

--- example/index.tsx
+++ example/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Spinner } from 'finity-ui';
+import Spinner from 'finity-ui';
 const App = () => {
   return (
     <>
